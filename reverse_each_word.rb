8910068f3e3a878933333a8b65bdc5a9e88369eb
Stephen Argentina <stephena9871@gmail.com>
2018-11-09 00:03:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(string)
   list = string.strip
-  newlist = []
-  for i in list
-    newlist.push(i.reverse)
-  end
+  newlist = list.collect {|i| i.reverse}
   new_string = newlist.join(" ")
   return new_string
 end
